3f60bb820e604d199644d58f2f86c5d6e35abd22
Ruben Nuñez Gonzalez <rnunezgonzalez@danielcastelao.org>
2022-10-21 09:41:12 +0200

    Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-
+        Funsiones calcSuma = new Funsiones();
+        int num1 = Integer.parseInt(JOptionPane.showInputDialog("Introduzca un numero: "));
+        int numb = Integer.parseInt(JOptionPane.showInputDialog("Introduzca otro numero: "));
+        System.out.println("La suma es: " + calcSuma.suma(num1,numb));
     }
 }
